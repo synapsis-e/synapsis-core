838d22781e41554b481f4f567544009fc19ca2a6
Kirill Sedyshev <agcrinax@gmail.com>
2024-01-02 04:07:29 +0300

    feat(all): add ...

--- src/crypto/sign.rs
+++ src/crypto/sign.rs
@@ -1,5 +1,5 @@
 use secp256k1::rand::rngs::OsRng;
-use secp256k1::{Secp256k1, SecretKey, PublicKey, Message, All};
+use secp256k1::{Secp256k1, SecretKey, PublicKey, Message, All, ecdsa};
 use secp256k1::hashes::sha256;
 use secp256k1::ecdsa::Signature;
 
@@ -10,20 +10,22 @@ pub struct Signed(
     SecretKey
 );
 
-pub struct Sign {
-    secp: Secp256k1<All>,
+#[derive(Debug)]
+pub enum SignError {
+    InvalidMessage,
+    InvalidPublicKey,
+    InvalidSignature,
+    IncorrectSignature,
+    
 }
 
-impl Sign {
-    pub fn new() -> Self {
-        Self {
-            secp: Secp256k1::new(),
-        }
-    }
+pub struct Sign;
 
+impl Sign {
     pub fn sign(&self, text: &[u8])
         -> Signed
     {
+        let secp = Secp256k1::signing_only();
         let (private_key, public_key) = self.secp.generate_keypair(&mut OsRng);
         let message = Message::from_hashed_data::<sha256::Hash>(
             text
@@ -31,5 +33,28 @@ impl Sign {
         let signature = self.secp.sign_ecdsa(&message, &private_key);
 
         Signed(signature, public_key, private_key)
+    }
+
+    pub fn verify(&self, text: &[u8], signature: &[u8], public: &[u8]) -> _ {
+        let secp = Secp256k1::verification_only();
+        let message = Message::from_digest_slice(text).map_err(|_| SignError::InvalidMessage)?;
+        let public_key = PublicKey::from_slice(public).map_err(|_| SignError::InvalidPublicKey)?;
+        let signature = ecdsa::Signature::from_compact(signature).map_err(|_| SignError::InvalidSignature)?;
+
+        secp.verify_ecdsa(&message, &signature, &public_key)
+            .map_err(|err| match err {
+                secp256k1::Error::IncorrectSignature => SignError::IncorrectSignature,
+                secp256k1::Error::InvalidMessage => SignError::InvalidMessage,
+                secp256k1::Error::InvalidPublicKey => SignError::InvalidPublicKey,
+                secp256k1::Error::InvalidSignature => SignError::InvalidSignature,
+                secp256k1::Error::InvalidSecretKey => todo!(),
+                secp256k1::Error::InvalidSharedSecret => todo!(),
+                secp256k1::Error::InvalidRecoveryId => todo!(),
+                secp256k1::Error::InvalidTweak => todo!(),
+                secp256k1::Error::NotEnoughMemory => todo!(),
+                secp256k1::Error::InvalidPublicKeySum => todo!(),
+                secp256k1::Error::InvalidParityValue(_) => todo!(),
+                secp256k1::Error::InvalidEllSwift => todo!(),
+            })
     }
 }
