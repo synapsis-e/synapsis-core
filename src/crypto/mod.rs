e330332522ee4d430a69277d1ce8382a7d5416b1
Crinax <agreedeth@gmail.com>
2024-01-12 13:45:45 +0300

    refactor(crypto): add decryption and remove signing

--- src/crypto/mod.rs
+++ src/crypto/mod.rs
@@ -1,31 +1,58 @@
-mod sign;
+use ecies::{decrypt, encrypt, utils::generate_keypair};
 
-use ecies::{decrypt, encrypt, utils::generate_keypair, PublicKey, SecretKey};
+pub struct Encrypted(Vec<u8>, [u8; 65], [u8; 32]);
 
-use self::sign::{Sign, Signed};
+pub struct MessageKey(Vec<u8>, [u8; 32]);
+
+pub struct Decrypted(pub Vec<u8>);
+
+pub struct Crypto;
 
 #[derive(Debug)]
-pub struct Encrypted(Signed, Vec<u8>, [u8; 65], [u8; 32]);
+pub enum CryptoError {
+    InvalidSignature,
+    InvalidKey,
+    Encryption,
+    Unreachable
+}
 
-pub struct Crypto {
-    sign: Sign,
+impl MessageKey {
+    pub fn from_raw(message: Vec<u8>, key: [u8; 32]) -> MessageKey {
+        MessageKey(message, key)
+    }
+}
+
+impl From<Encrypted> for MessageKey {
+    fn from(value: Encrypted) -> Self {
+        MessageKey(value.0, value.2)
+    }
 }
 
 impl Crypto {
-    pub fn new() -> Self {
-        Self {
-            sign: Sign::new(),
-        }
+    pub fn encrypt(text: &[u8]) -> Result<Encrypted, CryptoError> {
+        let (secret, public) = generate_keypair();
+        let (crypto_secret, crypto_public) = (secret.serialize(), public.serialize());
+        let encrypted = encrypt(&crypto_public, text).map_err(|_| CryptoError::Encryption)?;
+
+        Ok(Encrypted(encrypted, crypto_public, crypto_secret))
     }
 
-    pub fn encrypt(&self, text: &[u8]) -> Encrypted {
-        let (secret, public) = generate_keypair();
-        let (s_secret, s_public) = (secret.serialize(), public.serialize());
-        let encrypted = encrypt(&s_public, text)
-            .unwrap();
+    pub fn decrypt(encrypted: MessageKey) -> Result<Decrypted, CryptoError> {
+        let decrypted = decrypt(&encrypted.1, &encrypted.0);
 
-        let signed = self.sign.sign(text);
-
-        Encrypted(signed, encrypted, s_public, s_secret)
+        Ok(
+            Decrypted(
+                    decrypted.map_err(|err| match err {
+                    ecies::SecpError::InvalidSignature => CryptoError::InvalidSignature,
+                    ecies::SecpError::InvalidPublicKey => CryptoError::Unreachable,
+                    ecies::SecpError::InvalidSecretKey => CryptoError::InvalidKey,
+                    ecies::SecpError::InvalidRecoveryId => CryptoError::Unreachable,
+                    ecies::SecpError::InvalidMessage => CryptoError::InvalidKey,
+                    ecies::SecpError::InvalidInputLength => CryptoError::Unreachable,
+                    ecies::SecpError::TweakOutOfRange => CryptoError::Unreachable,
+                    ecies::SecpError::InvalidAffine => CryptoError::Unreachable,
+                })?
+            )
+        )
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,20 @@ mod crypto;
 use crypto::Crypto;
 
 fn main() {
-    let crypto = Crypto::new();
-    println!("{:?}", crypto.encrypt("Hello, world!".as_bytes()));
+    let crypted = Crypto::encrypt("Hello, world!".as_bytes());
+
+    if crypted.is_err() {
+        println!("{:?}", crypted.err());
+        return;
+    }
+
+    let crypted = crypted.unwrap();
+    let decrypted = Crypto::decrypt(crypted.into());
+
+    if decrypted.is_err() {
+        println!("{:?}", decrypted.err());
+        return;
+    }
+
+    println!("Encrypted: {:?}", String::from_utf8(decrypted.unwrap().0))
 }
